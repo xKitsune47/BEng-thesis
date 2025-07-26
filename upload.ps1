56d306abd1f31e75ac556b67a3f6d1320e140910
xKitsune47 <franek.cy@wp.pl>
2025-07-26 20:07:56 +0200

    modify psowershell script to be more dynamic

--- upload.ps1
+++ upload.ps1
@@ -1,7 +1,8 @@
-$files = @("server.py", "time_sync.py", "display.py", "wifi_manager.py", "sensor.py", "location_service.py", "config_manager.py", "main.py")
+$projectDir = Get-Location
 
-foreach ($file in $files){
-  Write-Host "Uploading file $file to ESP..."
+Get-ChildItem -Filter *.py | ForEach-Object {
+  $file = $_.Name
+  Write-Host "Uploading file '$file' to ESP32..."
   python -m mpremote connect COM3 cp .\$file : *>$null
   Start-Sleep -Seconds 1.5
 }
