861d8d448f6e98a594b13c2851bc52bf84b3b617
xKitsune47 <franek.cy@wp.pl>
2025-07-27 19:24:03 +0200

    remove unnecessary stuff, change sleep time to 2 seconds

--- wifi_manager.py
+++ wifi_manager.py
@@ -3,22 +3,17 @@ import uasyncio as asyncio
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-connected = False
 
 async def connect_wifi(ssid, password):
-    """Łączy się z Wi-Fi"""
-    global connected
     wlan.connect(ssid, password)
 
     for _ in range(10):
         if wlan.isconnected():
             ip = wlan.ifconfig()[0]
             print(f"✅ Połączono! IP: {ip}")
-            connected = True
             return True
         print("⏳ Próba połączenia...")
-        await asyncio.sleep(1)
+        await asyncio.sleep(2)
 
     print("❌ Błąd połączenia!")
-    connected = False
     return False
